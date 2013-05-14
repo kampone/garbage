8ac6024f8ba7bda032faaf648ece530df7e0e86b
Vlad <kosmonavtmikki@gmail.com>
2013-05-14 13:15:10 +0300

    Update light.cpp
    убрал пока текстуру, что б было проще ошибки искать, вот пока что сделал

--- light.cpp
+++ light.cpp
@@ -1,8 +1,9 @@
 #include "light.h"
+#include <QGLWidget>
 
 const  GLfloat pi=3.1415926535897932384626433832795, k=pi/180;
 const GLuint np=36;
-const GLfloat R=0.3f;
+const GLfloat R=0.7f;
 const GLfloat step=pi/np;
 light::light(QWidget *parent) :
     QGLWidget(parent), m_xRotate(0), m_yRotate(0), m_xGo(0), m_yGo(0)
@@ -11,8 +12,6 @@ light::light(QWidget *parent) :
   m_qObj = gluNewQuadric();
 
 }
-
-
 
 void light::initializeGL()
 {
@@ -35,16 +34,8 @@ void light::initializeGL()
    glEnable(GL_LIGHTING);
    glEnable(GL_LIGHT0);
    glEnable(GL_DEPTH_TEST);
-   genTextures(); // создать текстуры
-   glEnable(GL_TEXTURE_2D); // установить режим двумерных текстур
 
-
-   getVerTexArrays(); // определить массив текстурных координат вершин
-
-
-   glEnableClientState(GL_TEXTURE_COORD_ARRAY); // активизация массива текстурных координат
 }
-
 
 void light::paintGL()
 {
@@ -55,190 +46,56 @@ void light::paintGL()
 
    glMatrixMode(GL_MODELVIEW);
    glLoadIdentity();
-   glTranslatef(m_xGo, m_yGo, 0.0);
 
-   glRotatef(m_xRotate, 1.0, 0.0, 0.0);
-   glRotatef(m_yRotate, 0.0, 1.0, 0.0);
+glTranslatef(m_xGo, -m_yGo, 0.0);
+
+glRotatef(m_xRotate,1.0f, 0,0);
+glRotatef(m_yRotate, 0, 1.0f,0);
+//glRotatef(m_yRotate, -m_yGo, m_xGo, 0.0f);
 
 
+
+//drawAxis();
 }
-
-/*void light::timerEvent(QTimerEvent *)
+void light::drawAxis() // построить оси координат
 {
+   glLineWidth(5.0f); // устанавливаю ширину линии приближённо в пикселях
+   // до вызова команды ширина равна 1 пикселю по умолчанию
 
-    m_xTimerRotate+=1;
-    updateGL();
-}*/
 
-void light::getVerTexArrays() // определить массив вершин и массив текстурных координат
-{
+   glBegin(GL_LINES); // построение линии
+      glVertex3f( 1.0f,  0.0f,  0.0f); // первая точка
+      glVertex3f(-1.0f,  0.0f,  0.0f); // вторая точка
+   glEnd();
 
-   GLfloat phi, theta; // углы фи и тэта
+   QColor halfGreen(0, 128, 0, 255);
+   qglColor(halfGreen);
+   glBegin(GL_LINES);
+      // ось y зеленого цвета
+      glVertex3f( 0.0f,  1.0f,  0.0f);
+      glVertex3f( 0.0f, -1.0f,  0.0f);
 
-   // двойной цикл по углам
-   for (GLuint i=0; i < 2*np+1; i++)
-   {
-      phi=i*step; // изменение угла фи
-
-      for (GLuint j=0; j < np+1; j++)
-      {
-         theta=j*step; // изменение угла тэта
-
-         if (j==0) // вершины "северный полюс"
-         {
-            if (i<2*np)
-            {
-               vecVertices.push_back(0.0f); // пространственная x-координата вершины (добавить в конец вектора)
-               vecVertices.push_back(0.0f); // пространственная y-координата вершины
-               vecVertices.push_back(R);    // пространственная z-координата вершины
-
-               vecTextures.push_back((phi+step/2)/(2*pi)); // текстурная x-координата вершины
-               vecTextures.push_back(1.0f);                // текстурная y-координата вершины
-            }
-         }
-         else
-         {
-            if (j<np) // вершины между северными и южными полюсами
-            {
-               if (i<2*np)
-               {
-                  vecVertices.push_back(R*sin(theta)*cos(phi));
-                  vecVertices.push_back(R*sin(theta)*sin(phi));
-                  vecVertices.push_back(R*cos(theta));
-
-                  vecTextures.push_back(phi/(2*pi));
-               }
-               else
-               {
-                  vecVertices.push_back(R*sin(theta));
-                  vecVertices.push_back(0.0f);
-                  vecVertices.push_back(R*cos(theta));
-
-                  vecTextures.push_back(1.0f);
-               }
-               vecTextures.push_back((pi-theta)/pi);
-            }
-            else // вершины "южный полюс"
-            {
-               if (i<2*np)
-               {
-                  vecVertices.push_back(0.0f);
-                  vecVertices.push_back(0.0f);
-                  vecVertices.push_back(-R);
-
-                  vecTextures.push_back((phi+step/2)/(2*pi));
-                  vecTextures.push_back(0.0f);
-               }
-            }
-         }
-      }
-   }
+      glColor4f(0.00f, 0.00f, 1.00f, 1.0f);
+      // ось z синего цвета
+      glVertex3f( 0.0f,  0.0f,  1.0f);
+      glVertex3f( 0.0f,  0.0f, -1.0f);
+   glEnd();
 }
-
-void light::getIndexArray() // определить массив индексов
-{
-   for (GLuint i=0; i < 2*np; i++)
-   {
-      for (GLuint j=0; j < np; j++)
-      {
-         if (j==0)
-         {
-            vecIndices.push_back(i*(np+1));
-            vecIndices.push_back(i*(np+1)+1);
-            if (i<(2*np-1))
-               vecIndices.push_back((i+1)*(np+1)+1);
-            else
-               vecIndices.push_back((i+1)*(np+1));
-         }
-         else
-         {
-            if (j<(np-1))
-            {
-               if (i<(2*np-1))
-               {
-                  vecIndices.push_back(i*(np+1)+j);
-                  vecIndices.push_back((i+1)*(np+1)+(j+1));
-                  vecIndices.push_back((i+1)*(np+1)+j);
-
-                  vecIndices.push_back(i*(np+1)+j);
-                  vecIndices.push_back(i*(np+1)+(j+1));
-                  vecIndices.push_back((i+1)*(np+1)+(j+1));
-               }
-               else
-               {
-                  vecIndices.push_back(i*(np+1)+j);
-                  vecIndices.push_back((i+1)*(np+1)+j);
-                  vecIndices.push_back((i+1)*(np+1)+(j-1));
-
-                  vecIndices.push_back(i*(np+1)+j);
-                  vecIndices.push_back(i*(np+1)+(j+1));
-                  vecIndices.push_back((i+1)*(np+1)+j);
-               }
-            }
-            else
-            {
-
-               vecIndices.push_back((i+1)*(np+1)-1);
-               if (i<(2*np-1))
-                  vecIndices.push_back((i+2)*(np+1)-2);
-               else
-                  vecIndices.push_back((i+2)*(np+1)-3);
-               vecIndices.push_back((i+1)*(np+1)-2);
-            }
-         }
-      }
-   }
-}
-void light::drawFigure() // построить фигуру
-{
-   // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // изобразить фигуру в виде каркаса
-   //glColor4f(0.00f, 0.00f, 1.00f, 1.0f); // синий цвет
-
-   glVertexPointer(3, GL_FLOAT, 0, vecVertices.begin()); // указываем, откуда нужно извлечь данные о массиве вершин
-   glTexCoordPointer(2, GL_FLOAT, 0, vecTextures.begin()); // указываем, откуда нужно извлечь данные о массиве текстурных координат
-   glDrawElements(GL_TRIANGLES, vecIndices.size(), GL_UNSIGNED_INT, vecIndices.begin()); // строим поверхности
-    //glPointSize(3.0f); // размер точки примерно в пикселях
-
-   // glDrawArrays(GL_POINTS, 0, vecVertices.size()/3); // изобразить массив в виде точек
-}
-
-
-//...
-
-void light::genTextures() // функция genTexture() класса Scene3D, создаёт текстуры
-{
-   // загрузка изображений
-  /* QImage image1; // создаём объекты класса QImage (изображения)
-
-    image1.load("../textures/1.jpg"); // загружаем изображение в переменную image1
-
-   image1=QGLWidget::convertToGLFormat(image1); // конвертируем изображение в формат для работы с OpenGL
-
-
-   // создание имён для текстурных объектов
-   glGenTextures(1, textureID); // создаём два имени и записываем их в массив
-
-   // создаём и связываем текстурные объекты с состоянием текстуры
-   // 1-ый текстурный объект
-   glBindTexture(GL_TEXTURE_2D, textureID[1]); // создаём и связываем 1-ый текстурный объект с последующим состоянием текстуры
-   glTexImage2D(GL_TEXTURE_2D, 0, 3, (GLsizei)image1.width(), (GLsizei)image1.height(), 0, GL_RGBA, GL_UNSIGNED_BYTE, image1.bits()); // связываем текстурный объект с изображением
-
-
-   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); // задана линейная фильтрация вблизи
-   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR); // задана линейная фильтрация вдали
-   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); // при фильтрации игнорируются тексели, выходящие за границу текстуры для s координаты
-   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE); // при фильтрации игнорируются тексели, выходящие за границу текстуры для t координаты
-   glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE); // цвет текселя полностью замещает цвет фрагмента фигуры
-*/}
 void light::mousePressEvent(QMouseEvent *pe) {
     m_ptPosition = pe->pos();
 }
 
 void light::mouseMoveEvent(QMouseEvent *pe) {
-    m_xRotate += 180 * (GLfloat)(pe->y() - m_ptPosition.y()) / height();
+   /* m_xRotate += 180 * (GLfloat)(pe->y() - m_ptPosition.y()) / height();
     m_yRotate += 180 * (GLfloat)(pe->x() - m_ptPosition.x()) / width();
+
     m_xGo=(m_yRotate*pi*R)/180;
     m_yGo=-(m_xRotate*pi*R)/180;
+    */
+    m_yGo+=2 * (GLfloat)(pe->y() - m_ptPosition.y()) / height();
+    m_xGo+=2 * (GLfloat)(pe->x() - m_ptPosition.x()) / height();
+    m_yRotate=(m_xGo*180)/(pi*R);
+    m_xRotate=(m_yGo*180)/(pi*R);
     updateGL();
 
     m_ptPosition = pe->pos();
@@ -272,23 +129,36 @@ void light::keyPressEvent(QKeyEvent *pe){
         QApplication::exit();
         break;
     case Qt::Key_W:
-        m_xRotate-=2;
+        m_yGo-=0.03;
 
         break;
     case Qt::Key_S:
-        m_xRotate+=2;
+        m_yGo+=0.03;
         break;
     case Qt::Key_A:
-        m_yRotate-=2;
+        m_xGo-=0.03;
         break;
     case Qt::Key_D:
-        m_yRotate+=2;
+        m_xGo+=0.03;
+        break;
+    case Qt::Key_Space:
+        defaultScene();
         break;
     default:
         QWidget::keyPressEvent(pe);
     }
-    m_xGo=(m_yRotate*pi*R)/180;
-    m_yGo=-(m_xRotate*pi*R)/180;
+
+    m_yRotate=(m_xGo*180)/(pi*R);
+    m_xRotate=(m_yGo*180)/(pi*R);
     updateGL();
 
 }
+void light::defaultScene() // наблюдение сцены по умолчанию
+{
+   m_xRotate=0;
+   m_yRotate=0;
+   m_xGo=0;
+   m_yGo=0;
+}
+
+
